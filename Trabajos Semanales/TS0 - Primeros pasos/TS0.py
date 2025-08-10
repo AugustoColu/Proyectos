4c50ca19ebec6278396ff93845ce022dbed5555b
AugustoColu <augustoandres17@gmail.com>
2025-08-09 22:54:24 -0300

    Update TS0.py

--- Trabajos Semanales/TS0 - Primeros pasos/TS0.py
+++ Trabajos Semanales/TS0 - Primeros pasos/TS0.py
@@ -1,7 +1,18 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Aug  9 22:32:48 2025
+import matplotlib.pyplot as plt
+import numpy as np
 
-@author: Tuti
-"""
+fs = 500 # defino la frecuencia de la onda
+N = fs
+df = fs/N
+paso = 1/fs # defino el paso
+tiempo_simu = N*paso
 
+tt = np.arange (0, tiempo_simu, paso) # defino una sucesión de puntos en el tiempo
+
+fx = 10
+ax = 2
+
+xx = ax * np.sin (2*np.pi*fx*tt) # tt es un vector, por ende la función sin se evalúa para cada punto del mismo
+# xx tendrá la misma dimensión que tt
+
+plt.plot (tt, xx)
