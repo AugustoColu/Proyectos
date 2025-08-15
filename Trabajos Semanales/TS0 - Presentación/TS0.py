26e904ac429cd785ed12f5ac83a4ac6f2ffddcc3
AugustoColu <augustoandres17@gmail.com>
2025-08-15 20:03:10 -0300

    ts0

--- Trabajos Semanales/TS0 - Presentación/TS0.py
+++ Trabajos Semanales/TS0 - Presentación/TS0.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.signal as sp
 
 def func_senoidal (a_max, frec, fase, cant_muestras, frec_muestreo, v_medio):
     
@@ -24,44 +25,66 @@ v_medio = 0
 
 tt_1, ss_1 = func_senoidal (a_max = 1, frec = 10, fase = 0, cant_muestras = 1000, frec_muestreo = 1000, v_medio = 0)
 
-plt.subplot (4, 1, 1)
+plt.subplot (6, 1, 1)
 plt.plot (tt_1, ss_1, linestyle='-', color='black')
-plt.title ("Onda Senoidal 1")
-#plt.xlabel ("Timepo")
-#plt.ylabel ("Volts")
+plt.title ("Onda senoidal de 10 Hz")
+plt.xlabel ("Tiempo")
+plt.ylabel ("Volts")
 plt.grid (True)
 
 ### Señal 2 ###
 
 tt_2, ss_2 = func_senoidal (a_max = 1, frec = 500, fase = 0, cant_muestras = 1000, frec_muestreo = 1000, v_medio = 0)
 
-plt.subplot (4, 1, 2)
+plt.subplot (6, 1, 2)
 plt.plot (tt_2, ss_2, linestyle='-', color='black')
-plt.title ("Onda Senoidal 2")
-#plt.xlabel ("Timepo")
-#plt.ylabel ("Volts")
+plt.title ("Onda senoidal de 500 Hz")
+plt.xlabel ("Tiempo")
+plt.ylabel ("Volts")
 plt.grid (True)
 
 ### Señal 3 ###
 
 tt_3, ss_3 = func_senoidal (a_max = 1, frec = 999, fase = 0, cant_muestras = 1000, frec_muestreo = 1000, v_medio = 0)
 
-plt.subplot (4, 1, 3)
+plt.subplot (6, 1, 3)
 plt.plot (tt_3, ss_3, linestyle='-', color='black')
-plt.title ("Onda Senoidal 3")
-#plt.xlabel ("Timepo")
-#plt.ylabel ("Volts")
+plt.title ("Onda senoidal de 999 Hz")
+plt.xlabel ("Tiempo")
+plt.ylabel ("Volts")
 plt.grid (True)
 
 ### Señal 4 ###
 
 tt_4, ss_4 = func_senoidal (a_max = 1, frec = 1001, fase = 0, cant_muestras = 1000, frec_muestreo = 1000, v_medio = 0)
 
-plt.subplot (4, 1, 4)
+plt.subplot (6, 1, 4)
 plt.plot (tt_4, ss_4, linestyle='-', color='black')
-plt.title ("Onda Senoidal 4")
-#plt.xlabel ("Timepo")
-#plt.ylabel ("Volts")
+plt.title ("Onda senoidal de 1001 Hz")
+plt.xlabel ("Tiempo")
+plt.ylabel ("Volts")
+plt.grid (True)
+
+### Señal 5 ###
+
+tt_5, ss_5 = func_senoidal (a_max = 1, frec = 2001, fase = 0, cant_muestras = 1000, frec_muestreo = 1000, v_medio = 0)
+
+plt.subplot (6, 1, 5)
+plt.plot (tt_5, ss_5, linestyle='-', color='black')
+plt.title ("Onda senoidal de 2001 Hz")
+plt.xlabel ("Tiempo")
+plt.ylabel ("Volts")
+plt.grid (True)
+
+### Señal 6 ###
+
+ss_6 = sp.sawtooth (2 * np.pi * tt_1 * 10, width = 0.5)
+
+plt.subplot (6, 1, 6)
+plt.plot (tt_1, ss_6, linestyle='-', color='black')
+plt.title ("Onda diente de sierra de 10 Hz")
+plt.xlabel ("Tiempo")
+plt.ylabel ("Volts")
 plt.grid (True)
 
 plt.show ()
