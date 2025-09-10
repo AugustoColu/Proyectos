174fd554a0d52f08c9b6c4bcb82c6d000f5af0c2
AugustoColu <augustoandres17@gmail.com>
2025-09-10 15:46:06 -0300

    ventanas

--- Trabajos Semanales/TS3 - FFT, desparramo, interpol. y ventaneo/TS3.py
+++ Trabajos Semanales/TS3 - FFT, desparramo, interpol. y ventaneo/TS3.py
@@ -34,14 +34,17 @@ tt = eje_temporal (N, fs)
 nn = np.arange (N) # vector adimensional de muestras
 ff = np.arange (N) * df # vector en frecuencia al escalar las muestras por la resolución espectral
 
-x_1 = func_senoidal (tt = tt, amp = 1, frec = (N/4)*df, fase = 0, v_medio = 0, SNR = None) 
+x_1 = func_senoidal (tt = tt, amp = 1, frec = (N/4)*df, fase = 0, v_medio = 0, SNR = None)
+x_1 = x_1 / np.std(x_1) # potencia normalizada, idem. a dividir por np.sum(x_1**2)
 # observar que (N/4)*df = (N/4)*(fs/N) = fs/4, por ende no importa la cantidad de muestras, siempre la frecuencia será N/4
 X_1 = fft (x_1)
 
 x_2 = func_senoidal (tt = tt, amp = 1, frec = ((N/4)+0.25)*df, fase = 0, v_medio = 0, SNR = None)
+x_2 = x_2 / np.std(x_2)
 X_2 = fft (x_2)
 
 x_3 = func_senoidal (tt = tt, amp = 1, frec = ((N/4)+0.5)*df, fase = 0, v_medio = 0, SNR = None)
+x_3 = x_3 / np.std(x_3)
 X_3 = fft (x_3)
 
 
@@ -49,26 +52,18 @@ X_3 = fft (x_3)
 
 plt.figure (1)
 
-plt.subplot (3, 1, 1)
+plt.subplot (2, 1, 1)
 #plt.plot (nn, x_1, color='black') # eje de abscisas adimensional (cantidad de muestras)
-plt.plot (nn*df, x_1, color='black')
+plt.plot (nn, x_1, color='black')
 plt.title ("Señal de frecuencia N/4 en tiempo")
 plt.xlabel ("Tiempo [seg]")
 plt.ylabel ("Amplitud [V]")
-plt.xlim (0, fs/2) # ...visualizo hasta Nyquist
+plt.xlim (0, N/2) # ...visualizo hasta Nyquist
 plt.grid (True)
 
-plt.subplot (3, 1, 2)
+plt.subplot (2, 1, 2)
 #plt.plot (ff, np.abs(X), color='black') # Eje de ordenadas adimensional (N/2)
-plt.plot (ff, 10*np.log10(np.abs(X_1)), color='orange') # Eje de ordenadas adimensional en dB
-plt.title ("Módulo de la señal en frecuencia")
-plt.xlabel ("Frecuencia [Hz]")
-plt.ylabel ("[dB]")
-plt.xlim (0, fs/2)
-plt.grid (True)
-
-plt.subplot (3, 1, 3)
-plt.plot (ff, 10*np.log10(np.abs(X_1)**2), color='orange')
+plt.plot (ff, 10*np.log10(np.abs(X_1)**2), color='orange') # Eje de ordenadas adimensional en dB
 plt.title ("Módulo de la señal en frecuencia")
 plt.xlabel ("Frecuencia [Hz]")
 plt.ylabel ("[dB]")
@@ -83,15 +78,15 @@ plt.tight_layout ()
 plt.figure (2)
 
 plt.subplot (2, 1, 1)
-plt.plot (nn*df, x_2, color='black')
+plt.plot (nn, x_2, color='black')
 plt.title ("Señal de frecuencia N/4 + 0.25 en tiempo")
 plt.xlabel ("Tiempo [seg]")
 plt.ylabel ("Amplitud [V]")
-plt.xlim (0, fs/2) # ...visualizo hasta Nyquist
+plt.xlim (0, N/2) # ...visualizo hasta Nyquist
 plt.grid (True)
 
 plt.subplot (2, 1, 2)
-plt.plot (ff, 10*np.log10(np.abs(X_2)), color='orange')
+plt.plot (ff, 10*np.log10(np.abs(X_2)**2), color='orange')
 plt.title ("Módulo de la señal en frecuencia")
 plt.xlabel ("Frecuencia [Hz]")
 plt.ylabel ("[dB]")
@@ -106,15 +101,15 @@ plt.tight_layout ()
 plt.figure (3)
 
 plt.subplot (2, 1, 1)
-plt.plot (nn*df, x_3, color='black')
+plt.plot (nn, x_3, color='black')
 plt.title ("Señal de frecuencia N/4 + 0.5 en tiempo")
 plt.xlabel ("Tiempo [seg]")
 plt.ylabel ("Amplitud [V]")
-plt.xlim (0, fs/2) # ...visualizo hasta Nyquist
+plt.xlim (0, N/2) # ...visualizo hasta Nyquist
 plt.grid (True)
 
 plt.subplot (2, 1, 2)
-plt.plot (ff, 10*np.log10(np.abs(X_3)), color='orange')
+plt.plot (ff, 10*np.log10(np.abs(X_3)**2), color='orange')
 plt.title ("Módulo de la señal en frecuencia")
 plt.xlabel ("Frecuencia [Hz]")
 plt.ylabel ("[dB]")
@@ -126,30 +121,27 @@ plt.tight_layout ()
 plt.show ()
 
 
+### Verifico potencia unitaria de cada PSD por Parseval ###
 
-# %% Parseval
+# x_norm = (x - np.mean(x)) / (np.var(x))**(1/2)
+# # x_norm = func_senoidal (tt=tt, amp=np.sqrt(2), frec=100, fase=0, v_medio=0)
+# print ("Varianza =", np.var(x_norm), " ->  SD =", np.std(x_norm), " ->  Media =", np.mean(x_norm))
 
-x_norm = (x - np.mean(x)) / (np.var(x))**(1/2)
-# x_norm = func_senoidal (tt=tt, amp=np.sqrt(2), frec=100, fase=0, v_medio=0)
-print ("Varianza =", np.var(x_norm), " ->  SD =", np.std(x_norm), " ->  Media =", np.mean(x_norm))
-
-### Verifico Parseval ###
-
-A = np.sum ((np.abs(x))**2)
-B = np.sum ((np.abs(X))**2) / N
-print (A-B)
+pot_x1 = np.sum (np.abs(x_1)**2)
+pot_X1 = np.sum (np.abs(X_1)**2)/N
+print (pot_x1, pot_X1)
 
 # %% Zero-Padding
 
-zeros = np.zeros (len(x)*9)
-xPadding = np.concatenate ((x, zeros))
+zeros = np.zeros (len(x_1)*9)
+xPadding = np.concatenate ((x_1, zeros))
 XPadding = fft (xPadding)
 
 #ttPadding = eje_temporal (N = 10*N, fs = 1000)
 ttPadding = np.arange (10*N) * (fs / (10*N))
 
 plt.plot (ttPadding, 10*np.log10(np.abs(XPadding)), linestyle='', marker='x')
-plt.plot (ff, 10*(np.log10(np.abs(X))))
-# plt.xlim (0, 500)
+plt.plot (ff, 10*np.log10(np.abs(X_1)**2))
+plt.xlim (0, fs/2)
 plt.grid (True)
 plt.show ()
--- Trabajos Semanales/Ventanas.py
+++ Trabajos Semanales/Ventanas.py
@@ -24,32 +24,105 @@ tt = eje_temporal (N, fs)
 nn = np.arange (N) # vector adimensional de muestras
 ff = np.arange (N) * df # vector en frecuencia al escalar las muestras por la resolución espectral
 
-x = func_senoidal (tt = tt, amp = 1, frec = 25, fase = 0, v_medio = 0)
+x = func_senoidal (tt = tt, amp = 1, frec = 10, fase = 0, v_medio = 0)
+
+
+### Ventana tipo BlackmanHarris ###
 
 w_1 = sp.windows.blackmanharris (N)
-W_1 = fft (w_1, 2048) # esto calcula la FFT de w_1 con 2048 puntos de Zero-Padding
-X = fft (x*w_1, 2048)
+W_1 = fft (w_1, 2048)
 # observar que con solo W_1 = fft(w_1) no veo un carajo
+X_1 = fft (x*w_1, 2048) # esto calcula la FFT de x con 2048 puntos de Zero-Padding
+
 eje = np.linspace (0, fs, len(W_1)) # defino los límites que quiero y reparto el intervalo en len(W_1) partes iguales
 
 W_1dB = 20*np.log10(np.abs(fftshift(W_1))) # fftshift simplemente reordena los puntos de la FFT para centrarla en el gráfico
-X = 20*np.log10(np.abs(fftshift(X)))
+X_1dB = 20*np.log10(np.abs(fftshift(X_1)))
+
+plt.figure (1)
 
 plt.subplot (2, 1, 1)
-plt.plot (ff, x, color='black')
-plt.plot (ff, w_1, color='orange')
-plt.title ("Ventana en tiempo")
+plt.plot (ff, x, color='black', label='x')
+plt.plot (ff, w_1, color='orange', label='w_1')
+plt.title ("BlackmanHarris en tiempo")
 plt.xlabel ("Frecuencia [Hz]")
 plt.ylabel ("Amplitud [V]")
+plt.legend ()
 plt.grid (True)
 
 plt.subplot (2, 1, 2)
-plt.plot (eje, X, color='black')
-plt.plot (eje, W_1dB, color='orange')
+plt.plot (eje, X_1dB, color='black', label='X_1dB')
+plt.plot (eje, W_1dB, color='orange', label='W_1dB')
 plt.title ("Ventana en frecuencia")
 plt.xlabel ("Frecuencia [Hz]")
 plt.ylabel ("[dB]")
-plt.grid ()
+plt.legend ()
+plt.grid (True)
 
 plt.tight_layout ()
+
+
+### Ventana tipo FlatTop ###
+
+w_2 = sp.windows.flattop (N)
+W_2 = fft (w_2, 2048)
+X_2 = fft (x*w_2, 2048) # esto calcula la FFT de x con 2048 puntos de Zero-Padding
+
+W_2dB = 20*np.log10(np.abs(fftshift(W_2)))
+X_2dB = 20*np.log10(np.abs(fftshift(X_2)))
+
+plt.figure (2)
+
+plt.subplot (2, 1, 1)
+plt.plot (ff, x, color='black', label='x')
+plt.plot (ff, w_2, color='orange', label='w_2')
+plt.title ("FlatTop en tiempo")
+plt.xlabel ("Frecuencia [Hz]")
+plt.ylabel ("Amplitud [V]")
+plt.legend ()
+plt.grid (True)
+
+plt.subplot (2, 1, 2)
+plt.plot (eje, X_2dB, color='black', label='x_2dB')
+plt.plot (eje, W_2dB, color='orange', label='W_2dB')
+plt.title ("Ventana en frecuencia")
+plt.xlabel ("Frecuencia [Hz]")
+plt.ylabel ("[dB]")
+plt.legend ()
+plt.grid (True)
+
+plt.tight_layout ()
+
+
+### Ventana tipo Hamming ###
+
+w_3 = sp.windows.hamming (N)
+W_3 = fft (w_3, 2048)
+X_3 = fft (x*w_3, 2048) # esto calcula la FFT de x con 2048 puntos de Zero-Padding
+
+W_3dB = 20*np.log10(np.abs(fftshift(W_3)))
+X_3dB = 20*np.log10(np.abs(fftshift(X_3)))
+
+plt.figure (3)
+
+plt.subplot (2, 1, 1)
+plt.plot (ff, x, color='black', label='x')
+plt.plot (ff, w_3, color='orange', label='w_3')
+plt.title ("Hamming en tiempo")
+plt.xlabel ("Frecuencia [Hz]")
+plt.ylabel ("Amplitud [V]")
+plt.legend ()
+plt.grid (True)
+
+plt.subplot (2, 1, 2)
+plt.plot (eje, X_3dB, color='black', label='x_3dB')
+plt.plot (eje, W_3dB, color='orange', label='W_3dB')
+plt.title ("Ventana en frecuencia")
+plt.xlabel ("Frecuencia [Hz]")
+plt.ylabel ("[dB]")
+plt.legend ()
+plt.grid (True)
+
+plt.tight_layout ()
+
 plt.show ()
